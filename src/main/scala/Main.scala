f031ad682b6bc2ed2b5fc2b11937ede5ec109237
lilnho <nathanho2048@gmail.com>
2024-03-11 19:39:44 -0700

    Initial Commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.SparkContext._
+import scala.io._
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import scala.collection._
+import org.apache.spark.SparkContext._
+import scala.io._
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.rdd._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import scala.collection._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
